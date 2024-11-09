2fad251dace09ac9963890e0a3b1fce4a37db36e
yacin <yacin.ha9@gmail.com>
2024-11-09 16:26:11 +0100

    add gazebo color and simple controller

---
+++ build/scara_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/moose/ros2_ws/src/scara_bringup
+# Build directory: /home/moose/ros2_ws/src/build/scara_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/scara_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/moose/ros2_ws/src/scara_description
+# Build directory: /home/moose/ros2_ws/src/build/scara_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
